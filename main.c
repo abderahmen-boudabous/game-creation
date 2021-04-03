5827337cf7923a917d6302492568e03697e8e449
abderahmen-boudabous <abderahmen.boudabous@esprit.tn>
2021-04-03 10:04:57 +0100

    MAJ main

---
+++ main.c
@@ -0,0 +1,36 @@
+int main()
+{
+                        
+     int done=1;
+     Enemie e1;
+SDL_Surface* switchFullScreen(void);
+
+  init_enemie(&e1,1);
+
+  SDL_Surface * screen = NULL;
+  SDL_Surface * Ennemie = NULL;
+
+
+  SDL_Rect posecran;
+  SDL_Rect positione;
+
+
+  posecran.x = 0;
+  posecran.y = 0;
+  position0.x = 1500;
+  position0.y = 500;
+  positione.x = 900;
+  positione.y = 500;
+
+
+
+
+
+ while(done)
+{ 
+
+void initEnnemi(Ennemi*e)
+void afficherEnnemi(Ennemi e, SDL_Surface * screen)
+void animerEnnemi( Ennemi * e)
+
+}
